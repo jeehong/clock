4149e31ada5bd6e61f21dcb754893d1e4cbb9a0f
jeehong <jeehong2015@gmail.com>
2016-08-02 23:11:56 +0800

    删除无效变量

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,14 @@
 
 int main(int argc, char *argv[])
 {
-
     QApplication app(argc, argv);
     QQuickView viewer;
-    digitalClock clock;
 
     viewer.rootContext()->setContextProperty("DigitalClock", new digitalClock);
     viewer.setSource(QUrl("qrc:/qml/qml/clock.qml"));
 
     app.connect(viewer.engine(),&QQmlEngine::quit, &QGuiApplication::quit);
 
-    //clock.show();
     viewer.show();
 
     return app.exec();
